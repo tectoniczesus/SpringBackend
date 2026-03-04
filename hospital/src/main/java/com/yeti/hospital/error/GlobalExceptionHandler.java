714e09e738e13cdfc82566ebe9c2af588baa5409
Kunal Soni <122208285+tectoniczesus@users.noreply.github.com>
2026-03-04 11:50:33 +0530

    "exception handler created"

--- hospital/src/main/java/com/yeti/hospital/error/GlobalExceptionHandler.java
+++ hospital/src/main/java/com/yeti/hospital/error/GlobalExceptionHandler.java
@@ -1,16 +1,43 @@
 package com.yeti.hospital.error;
 
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<ApiError> handleUsernameNotFoundException(UsernameNotFoundException e){
-     ApiError apiError = new ApiError("Username not found with username" + e.getMessage(), HttpStatus.NOT_FOUND);
+     ApiError apiError = new ApiError("Username not found with username " + e.getMessage(), HttpStatus.NOT_FOUND);
       return new ResponseEntity<>(apiError, apiError.getStatusCode());
+    }
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleAuthenticationException(AuthenticationException e){
+        ApiError apiError = new ApiError("Authentication failed " + e.getMessage(), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError, apiError.getStatusCode());
+    }
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException e){
+        ApiError apiError = new ApiError("Invalid jwt token " + e.getMessage(), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError, apiError.getStatusCode());
+    }
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiError> handleAccessDeniedException(AccessDeniedException e){
+        ApiError apiError = new ApiError("Access denied: Insufficient permission " + e.getMessage(), HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(apiError, apiError.getStatusCode());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleGenericException(Exception e){
+        ApiError apiError = new ApiError("An unexcepted error occurred " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiError, apiError.getStatusCode());
     }
 
 }
